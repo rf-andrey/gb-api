058971a80563806e8a53bfd3adc7a9e06fb396f6
Andrey <andreyfontoura95@gmail.com>
2024-06-15 11:34:11 -0300

    feat: aplica hash na senha

--- src/database/prisma-client.ts
+++ src/database/prisma-client.ts
@@ -1,3 +1,19 @@
 import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
 
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient().$extends({
+  query: {
+    customer: {
+      $allOperations({ operation, args, query }) {
+        if (
+          (operation === "create" || operation === "update") &&
+          args.data["password"] &&
+          typeof args.data["password"] === "string"
+        ) {
+          args.data["password"] = bcrypt.hashSync(args.data["password"], 10);
+        }
+        return query(args);
+      },
+    },
+  },
+});
